b7a1b4a15de306003ffab0a4198711bf57fd7bc1
abhixnv <mabhinav534@gmail.com>
2022-09-18 17:15:22 +0530

    V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-eval */
+import { useEffect,useState } from "react";
 
 function App() {
+  const [cal, setCal] = useState("");
+  const [res, setRes] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+  const TITLE = "Calculator";
+
+  useEffect(() => { document.title = TITLE; });
+
+  const updateCal = (value) => {
+    if ((ops.includes(value) && cal === "") || (ops.includes(value) && ops.includes(cal.slice(-1)))) {
+      return;
+    }
+
+    setCal(cal + value);
+
+    if (!ops.includes(value)) {
+      setRes(eval(cal+value).toString());
+    }
+  };
+
+  const calculate = () => {
+    setCal(eval(cal).toString());
+  };
+
+  const deleteCal = () => {
+    if ( cal === "") {
+      return;
+    }
+
+    const value = cal.slice(0, -1);
+
+    setCal(value);
+    setRes(value);
+  };
+
+  const createDig = () => {
+    const digits = [];
+    for (let i = 1; i< 10; i++) {
+      digits.push(
+          <button onClick={ () => updateCal(i.toString()) } key={i}>{i}</button>,
+      );
+    }
+    return digits;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          { res ? <span>({res})</span> : ""}&nbsp;
+          { cal || "0" }
+        </div>
+        <div className="operators">
+          <button onClick={ () => updateCal("/") }>/</button>
+          <button onClick={ () => updateCal("*") }>*</button>
+          <button onClick={ () => updateCal("+") }>+</button>
+          <button onClick={ () => updateCal("-") }>-</button>
+
+          <button onClick={deleteCal}>DEL</button>
+        </div>
+        <div className="digits">
+          { createDig() }
+          <button onClick={ () => updateCal("0") }>0</button>
+          <button onClick={ () => updateCal(".") }>.</button>
+
+          <button className="equal" onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
